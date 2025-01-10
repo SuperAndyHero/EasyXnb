5827b5705f4e46c90c6ebc8b5ffe8a3bc38ed896
SuperAndyHero <31840036+SuperAndyHero@users.noreply.github.com>
2025-01-10 15:46:46 -0600

    logger for when fbx importer throws a warning

---
+++ EasyXnb/BuildLogger.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xnbcompiler.EasyXnb
+{
+    public sealed class BuildLogger : ContentBuildLogger
+    {
+        public override void LogMessage(string message, params object[] messageArgs) 
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Log: " + message, messageArgs);
+        }
+
+        public override void LogImportantMessage(string message, params object[] messageArgs) 
+        {
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Log Important: " + message, messageArgs);
+        }
+
+        public override void LogWarning(string helpLink, ContentIdentity contentIdentity, string message, params object[] messageArgs) 
+        {
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Warning: " + message + ", at: ", messageArgs);
+            Console.WriteLine("Warning type: " + helpLink);
+            Console.WriteLine("Source file name: " + contentIdentity.SourceFilename);
+            Console.WriteLine("Source tool: " + contentIdentity.SourceTool);
+            Console.WriteLine("Fragment identifier: " + contentIdentity.FragmentIdentifier);
+        }
+    }
+}
--- EasyXnb/DfgImporterContext.cs
+++ EasyXnb/DfgImporterContext.cs
@@ -1,35 +1,19 @@
 using System;
 using Microsoft.Xna.Framework.Content.Pipeline;
+using xnbcompiler.EasyXnb;
 
 namespace DynamicFontGenerator
 {
 	public sealed class DfgImporterContext : ContentImporterContext
 	{
-		public override ContentBuildLogger Logger
-		{
-			get
-			{
-				throw new NotImplementedException();
-			}
-		}
+		public BuildLogger BuildLogger = new BuildLogger();
+		public override ContentBuildLogger Logger => BuildLogger;
 
-		public override string OutputDirectory
-		{
-			get
-			{
-				throw new NotImplementedException();
-			}
-		}
+        public override string OutputDirectory => GeneratorGame.outputDirectorySetting;
 
-		public override string IntermediateDirectory
-		{
-			get
-			{
-				return GeneratorGame.intermedDirectorySetting;
-			}
-		}
+        public override string IntermediateDirectory => GeneratorGame.intermedDirectorySetting;
 
-		public override void AddDependency(string filename)
+        public override void AddDependency(string filename)
 		{
 			throw new NotImplementedException();
 		}
